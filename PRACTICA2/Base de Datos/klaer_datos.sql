f99f4b5a4a49ba178d018be6692eae082365b4ad
DanielOrtizDelgado <79509029+DanielOrtizDelgado@users.noreply.github.com>
2023-03-09 14:55:19 +0100

    Add files via upload

---
+++ PRACTICA2/Base de Datos/klaer_datos.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-03-2023 a las 14:41:14
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `klaer`
+--
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT DELAYED IGNORE INTO `roles` (`ID`, `Nombre`) VALUES
+(1, 'admin'),
+(2, 'mod'),
+(3, 'user');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
