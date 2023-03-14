e6d4e210d622ceaeb20c1fb4434310bca9b5734b
gmartinezcarrera <gonzalomartinezc98@gmail.com>
2023-03-14 17:11:50 +0100

    updated css and comprar/vender

--- PRACTICA2/vistas/vender.php
+++ PRACTICA2/vistas/vender.php
@@ -9,25 +9,40 @@ $tituloPagina = 'PáginaVentas';
 $inicio = true;
 
 
-$contenidoPrincipal=<<<EOS
-    <link rel="stylesheet" type="text/css" href='../css/estilo.css' />
-	<h1>VENTAS</h1>
-    <button class="open-button" onclick="openForm()">Chat</button>
+$contenidoPrincipal = <<<EOS
+<link rel="stylesheet" type="text/css" href='../css/estilo.css' />
+<h1>VENDER</h1>
 
-    <div class="chat-popup" id="myForm">
-         <form action="/action_page.php" class="form-container">
-            <h1>VENDER</h1>
+    <form action="../Aplicacion.php" class="form-container">
 
-            <label for="msg"><b>Message</b></label>
-            <textarea placeholder="Type message.." name="msg" required></textarea>
+        <fieldset>
+            <legend>Producto</legend>
+            <div><label>Name:</label> <input type="text" name="productName"  /></div>
+            <div><label>ID:</label> <input type="text" name="productId"  /></div>
+            <div><label>Price:</label> <input type="text" name="productPrice"  /></div>
+            <div><label>Description:</label> <input type="text" name="productDescr"  /></div>
+            <div><label>Type:</label> <input type="text" name="productType"  /></div>
+            <div><label>Date:</label> <input type="text" name="productDate"  /></div>
+            <div><label>Amount:</label> <input type="text" name="password"  /></div>
 
-            <button type="submit" class="btn">Send</button>
-            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-        </form>
-    </div>
-    
+            <div><button type="submit">Entrar</button></div>
+        </fieldset>
+
+        <!--aqui faltaría poner la función que va a hacer que se muestren los productos con ese nombre/id/categoría-->
+
+
+    </form>
+
 
 EOS;
+
+
+function buildFormularioProductUpload()
+{
+    return <<<EOS
+        <p>Prueba Formulario Product Upload </p>
+    EOS;
+}
 
 //require_once 'includes/config.php';
 require 'comun/lobby.php';
