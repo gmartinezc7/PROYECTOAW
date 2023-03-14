af439b8ebc74361e50db5653ca5e480f15257c5e
gmartinezcarrera <gonzalomartinezc98@gmail.com>
2023-03-14 14:27:09 +0100

    updated db conection

--- PRACTICA2/config.php
+++ PRACTICA2/config.php
@@ -17,8 +17,8 @@ define('BD_PASS', 'klaer');
  */
 
  // Cambiar
-define('RAIZ_APP', __DIR__);
-/*define('RUTA_IMGS', RUTA_APP.'/img');
+/*define('RAIZ_APP', __DIR__);
+define('RUTA_IMGS', RUTA_APP.'/img');
 define('RUTA_CSS', RUTA_APP.'/css');
 define('RUTA_JS', RUTA_APP.'/js');
 define('RUTA_VISTAS', RAIZ_APP.'/vistas');*/
@@ -26,9 +26,9 @@ define('RUTA_VISTAS', RAIZ_APP.'/vistas');*/
 /**
  * Configuración del soporte de UTF-8, localización (idioma y país) y zona horaria
  */
-ini_set('default_charset', 'UTF-8');
+/*ini_set('default_charset', 'UTF-8');
 setLocale(LC_ALL, 'es_ES.UTF.8');
-date_default_timezone_set('Europe/Madrid');
+date_default_timezone_set('Europe/Madrid');*/
 
 // Inicializa la aplicación
 //$app = Aplicacion::getInstance(); // Comprobar
--- PRACTICA2/index.php
+++ PRACTICA2/index.php
@@ -5,11 +5,25 @@ require_once 'config.php';
 $tituloPagina = 'Portada';
 $inicio = false;
 
-$contenidoPrincipal=<<<EOS
-<link rel="stylesheet" type="text/css" href='css/estilo.css' />
 
-<h1>Página principal</h1>
+
+$dni = '45678903';
+//Conexion con base de datos
+$conn = new mysqli(BD_HOST, BD_USER, BD_PASS, BD_NAME);
+
+
+$contenidoPrincipal=<<<EOS
+	<link rel="stylesheet" type="text/css" href='css/estilo.css' />
+	<p>**Conectando con la base de datos**</p>
+	<h1>Página principal</h1>
 	<p> PÁGINA DE INICIO DE KLAER </p>
 EOS;
+if ($conn->connect_error){
+    die("La conexión ha fallado" . $conn->connect_error);
+}else $contenidoPrincipal .= '<p>***La conexión es correcta***</p>';
+
+$conn->close();
+$contenidoPrincipal .= '<p>**Cerrando la conexión**</p>';
+
 
 require 'vistas/comun/lobby.php';
