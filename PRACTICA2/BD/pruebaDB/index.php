fc30596984fbe6b13972400a91515d7051e8b7a0
gmartinezcarrera <gonzalomartinezc98@gmail.com>
2023-03-14 10:11:18 +0100

    Update index.php

--- PRACTICA2/BD/pruebaDB/index.php
+++ PRACTICA2/BD/pruebaDB/index.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:PRACTICA2/BD/pruebaDB/index.php
 <?php
 //Definicion de constantes
 //Parametros de acceso de la base de datos
@@ -20,27 +19,4 @@ $contenidoPrincipal .= '<p>***La conexión es correcta***</p>';
 $conn->close();
 $contenidoPrincipal .= '<p>**Cerrando la conexión**</p>';
 
-=======
-<?php
-//Definicion de constantes
-//Parametros de acceso de la base de datos
-define('BD_HOST', 'localhost');
-define('BD_USER', 'klaer');
-define('BD_PASS', 'klaer');
-define('BD_NAME', 'klaer');
-
-$tituloPagina = 'Datos';
-$contenidoPie = 'No hay errores';
-$contenidoPrincipal = '<p>**Conectando con la base de datos**</p>';
-$dni = '45678903';
-//Conexion con base de datos
-$conn = new mysqli(BD_HOST, BD_USER, BD_PASS, BD_NAME);
-if ($conn->connect_error){
-    die("La conexión ha fallado \n" . $conn->connect_error);
-}
-$contenidoPrincipal .= '<p>***La conexión es correcta***</p>';
-$conn->close();
-$contenidoPrincipal .= '<p>**Cerrando la conexión**</p>';
-
->>>>>>> Stashed changes:PRACTICA2/Base de Datos/pruebaDB/index.php
 include __DIR__.'/includes/plantillas/plantilla.php';
