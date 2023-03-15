423d5fcdcb0d96e77ce8d199452435996a188310
majir96 <61943711+majir96@users.noreply.github.com>
2023-03-15 13:13:36 +0100

    Parte de Angela y Elo

--- PRACTICA2/vistas/login.php
+++ PRACTICA2/vistas/login.php
@@ -1,32 +1,17 @@
 <?php
 
-$tituloPagina = 'Página Login';
-$inicio = true;
+require_once '../config.php';
+require_once '../FormularioLogin.php';
 
+$tituloPagina = 'Login';
 
+$form = new FormularioLogin("login.php"); 
+$form->gestiona();
+$formulario = $form->generaFormulario();
 
-$contenidoPrincipal=<<<EOS
-    <link rel="stylesheet" type="text/css" href='../css/estilo.css' />
-	<h1>PÁGINA LOGIN</h1>
-    <div class="contenedor">
-        <p> Este es el espacio para el Login </p>
-       
-    </div>
-
+$contenidoPrincipal = <<<EOS
+	<h1>Acceso al sistema</h1>
+	$formulario
 EOS;
 
-function buildFormularioLogin($username='', $password='')
-{
-    return <<<EOS
-    <link rel="stylesheet" type="text/css" href='../css/estilo.css' />
-    <form id="formLogin" action="procesarLogin.php" method="POST">
-        <fieldset>
-            <legend>Usuario y contraseña</legend>
-            <div><label>Name:</label> <input type="text" name="username" value="$username" /></div>
-            <div><label>Password:</label> <input type="password" name="password" password="$password" /></div>
-            <div><button type="submit">Entrar</button></div>
-        </fieldset>
-    </form>
-    EOS;
-}
-require 'comun/lobby.php';
+require 'vistas/comun/lobby.php';
--- PRACTICA2/vistas/registrarse.php
+++ PRACTICA2/vistas/registrarse.php
@@ -1,35 +1,17 @@
 <?php
-$tituloPagina = 'Página Registro';
-$inicio = true;
 
+require_once '/config.php';
+require_once '../FormularioRegistro.php';
 
-$contenidoPrincipal=<<<EOS
-    <link rel="stylesheet" type="text/css" href='../css/estilo.css' />
-	<h1>PÁGINA REGISTRO</h1>
-    <div class="contenedor">
-        <p> Este es el espacio para el Registro </p>
-       
-    </div>
+$tituloPagina = 'Registro';
 
+$form = new FormularioRegistro(""); 
+$form->gestiona();
+$formulario = $form->generaFormulario();
+
+$contenidoPrincipal = <<<EOS
+	<h1>Registro de usuario</h1>
+	$formulario
 EOS;
 
-function buildFormularioRegistro($username='', $password='', $email='',$tfl='',$dir='',$suscrib='') // Hay que procesar el registro creandolo en la base de datos, ademas comprobar si ya existe
-{
-    return <<<EOS
-    <link rel="stylesheet" type="text/css" href='../css/estilo.css' />
-    <form id="formLogin" action="procesarRegistro.php" method="POST">
-        <fieldset>
-            <legend>Nuevo usuario</legend>
-            <div><label>Nombre de usuario:</label> <input type="text" name="username" value="$username" /></div>
-            <div><label>Contraseña:</label> <input type="password" name="password" password="$password" /></div>
-            <div><label>Email:</label> <input type="text" name="email" value="$email" /></div>
-            <div><label>Telefono:</label> <input type="text" name="tfl" value="$tfl" /></div>
-            <div><label>Dirección:</label> <input type="text" name="dir" value="$dir" /></div>
-            <div><label>Suscribirse:</label> <input type="checkbox" name="suscrib" valuez="$suscrib" /></div>
-            <div><button type="submit">Crear cuenta</button></div>
-        </fieldset>
-    </form>
-    EOS;
-}
-
-require 'comun/lobby.php';
+require 'vistas/comun/lobby.php';
