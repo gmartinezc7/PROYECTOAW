2b43c8343cffc434d313a8db95d46f130fe9dc9b
gmartinezcarrera <gonzalomartinezc98@gmail.com>
2023-03-16 23:39:19 +0100

    updated carrito

--- PRACTICA2/vistas/carrito.php
+++ PRACTICA2/vistas/carrito.php
@@ -1,15 +1,37 @@
 <?php
-/**require_once 'includes/config.php';
-require_once 'includes/vistas/helpers/autorizacion.php'; */
+
 
 $tituloPagina = 'Carrito';
 $inicio = true;
 
-$contenidoPrincipal=<<<EOS
-	<link rel="stylesheet" type="text/css" href='../css/estilo.css' />
-	<h1>CARRITO</h1>
-	<p> Carrito de compra de usuario logueado, si no hay usuario logueado, no se muestra</p>
-EOS;
+//session_start();
+$codigohtml2 = '';
 
+if(isset($_SESSION['usuario'])){
+    $contenidoPrincipal = <<<EOF
+    <p> Productos añadido(s) al carrito </p>
+    EOF;
+
+    if(isset($_POST['botonComprar'])){
+        $i = $_POST['indice'];
+        $productos = unserialize($_POST['prod']);
+
+        $prod_carrito = $productos[$i];
+        $nombre = $prod_carrito->getNombreProd();
+        $descripcion = $prod_carrito->getDescripcion();
+        $tipo = $prod_carrito->getNombreProd();
+        $fecha = $prod_carrito->getFecha();
+        $stock = $prod_carrito->getStock();
+
+        $contenidoPrincipal .= <<<EOF
+        <p> Nombre: $nombre Descripcion: $descripcion Categoría: $tipo Fecha: $fecha Cantidad disponible: $stock </p>
+        EOF;
+		echo "hola prueba de carrito22222s";
+    }
+} else {
+    $contenidoPrincipal = <<<EOF
+    <p> Hace falta estar logeado para observar el carrito </p>
+    EOF;
+}
 
 require 'comun/lobby.php';
--- PRACTICA2/vistas/comprar.php
+++ PRACTICA2/vistas/comprar.php
@@ -1,11 +1,10 @@
-
-
 <?php
 
 require '../Producto.php';
+//require 'carrito.php';
 //require ("./comun/pie.php"); 
-/**require_once 'includes/config.php';
-require_once 'includes/vistas/helpers/autorizacion.php'; */
+/*require_once 'includes/config.php';
+require_once 'includes/vistas/helpers/autorizacion.php';*/
 
 $tituloPagina = 'Página de Compras';
 $inicio = true;
@@ -14,7 +13,7 @@ $inicio = true;
 $busquedaFor = buildFormProds();
 $contenidoPrincipal=<<<EOS
     <link rel="stylesheet" type="text/css" href='../css/estilo.css' />
-	<h1>COMPRAR</h1>
+    <h1>COMPRAR</h1>
     $busquedaFor
 EOS;
 
@@ -23,7 +22,11 @@ EOS;
 function buildFormProds(){
     $codigohtml = '';
     $busquedaProductos = Producto::buscaDisponibles();
+    $prodsSerializado = serialize($busquedaProductos);
+    $i=0;
     foreach($busquedaProductos as $producto){
+
+
 
         $nombre = $producto->getNombreProd();
         $descripcion = $producto->getDescripcion();
@@ -31,17 +34,21 @@ function buildFormProds(){
         $fecha = $producto->getFecha();
         $stock = $producto->getStock();
 
-        
-       
+
         $codigohtml .= <<<EOS
-        <p> Nombre: $nombre Descripcion: $descripcion Categoría: $tipo Fecha: $fecha Cantidad disponible: $stock</p>
+        <p> Nombre: $nombre Descripcion: $descripcion Categoría: $tipo Fecha: $fecha Cantidad disponible: $stock </p>
+        <input type="hidden" name="prod" value="<?= htmlentities($prodsSerializado) ?">
+        <input type="hidden" name="indice" value="<?php echo $i; ?>">
+        <form action="carrito.php" class="form-container" method="POST">
+        <div><button type="submit" name="botonComprar">Comprar</button></div>
         EOS;
+
+        ++$i;
     }
 
     return $codigohtml;
 
 }
-
 
 
 
