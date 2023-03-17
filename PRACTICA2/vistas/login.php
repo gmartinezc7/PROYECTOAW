8aa6af8cb4e9ed538753192ef428c33fc0039908
DanielOrtizDelgado <79509029+DanielOrtizDelgado@users.noreply.github.com>
2023-03-17 16:46:07 +0100

    Update login.php

--- PRACTICA2/vistas/login.php
+++ PRACTICA2/vistas/login.php
@@ -2,17 +2,18 @@
 
 require_once '../config.php';
 require_once '../FormularioLogin.php';
+require_once '../Formulario.php';
 
 $tituloPagina = 'Login';
 $inicio = true;
 
 $form = new FormularioLogin("login.php"); 
-$form->gestiona();
-$formulario = $form->generaFormulario();
+$htmlFormLogin = $form->gestiona();
+//$formulario = $form->generaFormulario();
 
 $contenidoPrincipal = <<<EOS
 	<h1>Acceso al sistema</h1>
-	$formulario
+	$htmlFormLogin
 EOS;
 
 require __DIR__.'/comun/lobby.php';
