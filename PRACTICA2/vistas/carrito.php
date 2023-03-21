9122a2b617a84e6090b31bb622333db9f384c5c2
angela <124682167+alp1610@users.noreply.github.com>
2023-03-21 19:01:57 +0100

    Add files via upload

---
+++ PRACTICA2/vistas/carrito.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once "../Aplicacion.php";
+require_once "comprar.php";
+
+$tituloPagina = 'Carrito';
+$inicio = true;
+
+//session_start();
+$codigohtml2 = '';
+
+if(isset($_SESSION['login'])){
+    $contenidoPrincipal = <<<EOF
+    <p> Productos añadido(s) al carrito </p>
+    EOF;
+
+    if(isset($_POST['botonComprar'])){
+        $i = $_POST['indice'];
+        $productos = unserialize($_POST['prod']);
+
+        $prod_carrito = $productos[$i];
+        $nombre = $prod_carrito->getNombreProd();
+        $descripcion = $prod_carrito->getDescripcion();
+        $tipo = $prod_carrito->getNombreProd();
+        $fecha = $prod_carrito->getFecha();
+        $stock = $prod_carrito->getStock();
+
+        $contenidoPrincipal .= <<<EOF
+        <p> Nombre: $nombre Descripcion: $descripcion Categoría: $tipo Fecha: $fecha Cantidad disponible: $stock </p>
+        EOF;
+		echo "hola prueba de carrito22222s";
+    }
+} else {
+    $contenidoPrincipal = <<<EOF
+    <p> Hace falta estar logeado para observar el carrito </p>
+    EOF;
+}
+
+require 'comun/lobby.php';
